61e65ac8dd05fef417749defb53d0c2f3101d8da
Chaphasilor <ppp.u@web.de>
2021-03-18 19:55:52 +0100

    finished comments for index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ console.logLevel = process.env.environment === `development` ? 4 : 3 // level 4 
 // import helper classes
 const PacketParser = require(`./parse`)
 const GuiConnection = require(`./gui-connection`);
+const Packet = require('./packet');
 
 // create a http server to bind the websocket server to
 // the websocket server *could* create its own server, but then it can't shared a port with other http-server stuff
@@ -131,57 +132,52 @@ function handleCommand(socketId, command) {
     
       break;
 
-    case `live`:
+    // subscribes a client to live packets
+    case `packetsLive`:
 
-      // parser.off(`packet`, sendLivePacketSummary(socketId)) // make sure any previous handler is removed before attaching a new handler
-      // parser.on(`packet`, sendLivePacketSummary(socketId))
-
-      connection.subscribe(socketId, command[0])
+      connection.subscribe(socketId, command[0]) // adds a new subscription inside the GuiConnection, so that broadcasts for `packetsLive` are forwarded to this client
       
       break;
 
+    // subscribes a client to live connections
     case `connectionsLive`:
 
-      // parser.off(`new-connection`, sendConnections(socketId)) // make sure any previous handler is removed before attaching a new handler
-      // parser.on(`new-connection`, sendConnections(socketId))
-
-      connection.subscribe(socketId, command[0])
+      connection.subscribe(socketId, command[0]) // adds a new subscription inside the GuiConnection, so that broadcasts for `connectionsLive` are forwarded to this client
       
       break;
 
+    // sends all known connections that exist or have existed to the client
     case `connections`:
 
-      console.log(`parser.connections:`, parser.connections);
-      connection.send(socketId, response([...parser.connections.values()]))
+      console.debug(`parser.connections:`, parser.connections);
+      connection.send(socketId, response([...parser.connections.values()])) // `parser.connections` is a Map, so the spread operator (`...`) is used to convert it into an array
       connection.send(socketId, end())
 
       break;
 
+    // subscribes a client to live advertisers
     case `advertisersLive`:
 
-      // parser.off(`new-advertiser`, sendAdvertisers(socketId)) // make sure any previous handler is removed before attaching a new handler
-      // parser.on(`new-advertiser`, sendAdvertisers(socketId))
-
-      connection.subscribe(socketId, command[0])
+      connection.subscribe(socketId, command[0]) // adds a new subscription inside the GuiConnection, so that broadcasts for `advertisersLive` are forwarded to this client
       
       break;
 
+    // sends all known advertisers that exist or have existed to the client
     case `advertisers`:
 
-      connection.send(socketId, response([...parser.advertisers.values()]))
+      connection.send(socketId, response([...parser.advertisers.values()])) // `parser.advertisers` is a Map, so the spread operator (`...`) is used to convert it into an array
       connection.send(socketId, end())
 
       break;
 
+    // subscribes a client to live issues
     case `issuesLive`:
 
-      // parser.off(`new-issue`, sendIssues(socketId)) // make sure any previous handler is removed before attaching a new handler
-      // parser.on(`new-issue`, sendIssues(socketId))
-
-      connection.subscribe(socketId, command[0])
+      connection.subscribe(socketId, command[0]) // adds a new subscription inside the GuiConnection, so that broadcasts for `issuesLive` are forwarded to this client
       
       break;
 
+    // sends all detected issues to the client
     case `issues`:
 
       connection.send(socketId, response(parser.issues))
@@ -189,9 +185,10 @@ function handleCommand(socketId, command) {
 
       break;
 
+    // used to end a subscription to a live/broadcast command
     case `end`:
 
-      connection.unsubscribe(socketId, command[1])
+      connection.unsubscribe(socketId, command[1]) // unsubscribe the client inside GuiConnection so that broadcasts aren't forwarded anymore
       connection.send(socketId, response(`Success`))
       connection.send(socketId, end())
 
@@ -203,47 +200,55 @@ function handleCommand(socketId, command) {
   
 }
 
-parser.on(`packet`, addPacketToBroadcastQueue)
-// parser.on(`packet`, broadcastPacket)
-parser.on(`new-connection`, broadcastConnections)
-parser.on(`new-advertiser`, broadcastAdvertisers)
-parser.on(`new-issue`, broadcastIssues)
+parser.on(`packet`, addPacketToBroadcastQueue) // packets aren't broadcasted directly, it's more efficient to queue them up and send multiple packets at a regular interval
+parser.on(`new-connection`, broadcastConnections) // attach the broadcast handler for connections
+parser.on(`new-advertiser`, broadcastAdvertisers) // attach the broadcast handler for advertisers
+parser.on(`new-issue`, broadcastIssues) // attach the broadcast handler for issues
 
 let packetQueue = []
-
+// send packets at a regular interval. can be increased if packets are arriving to fast
 setInterval(() => {
 
-  // console.log(`Checking packet queue...`)
-  
   if (packetQueue.length > 0) {
     
-    // console.log(`packetQueue:`, packetQueue)
+    console.debug(`packetQueue:`, packetQueue)
     broadcastPackets(packetQueue)
-    packetQueue.length = 0
+    packetQueue.length = 0 // clear the packet queue, very important :)
     
   }
 
 }, 50)
 
+// also regularly update packet counts for connections and advertisers, because else they would only change if a new connection or advertiser is detected, which is rather infrequent 
 setInterval(() => {
 
-  broadcastConnections([...parser.connections.values()]) // broadcast this regularly to keep # of packets up to date
-  broadcastAdvertisers([...parser.advertisers.values()]) // broadcast this regularly to keep # of packets up to date
+  broadcastConnections([...parser.connections.values()]) // broadcast this regularly to keep number of packets inside the connection up to date
+  broadcastAdvertisers([...parser.advertisers.values()]) // broadcast this regularly to keep number of packets from different advertisers up to date
   
 }, 500)
 
+/**
+ * ### Handler for the `packet` event.  
+ * takes a single packet and adds the packet (simple format) to the broadcast queue
+ * @param {Packet} packet The packet emitted by the event
+ */
 function addPacketToBroadcastQueue(packet) {
   let simplePacket = packet.getInfo()
   packetQueue.push(simplePacket)
 }
 
+/**
+ * Can be used to directly broadcast a single packet  
+ * **not recommended** for medium to high frequency packet streams, but in theory offers the lowest latency
+ * @param {Packet} packet The packet emitted by the event
+ */
 function broadcastPacket(packet) {
   let simplePacket = packet.getSimpleInfo()
   try {
-    connection.broadcast(`live`, {
+    connection.broadcast(`packetsLive`, {
       type: `response`,
       value: [
-        `live`,
+        `packetsLive`,
         [simplePacket],
       ]
     })   
@@ -252,12 +257,16 @@ function broadcastPacket(packet) {
   }
 }
 
+/**
+ * ### Broadcast multiple packets at once
+ * @param {Array<Packet>} packets All packets to be broadcasted
+ */
 function broadcastPackets(packets) {
   try {
-    connection.broadcast(`live`, {
+    connection.broadcast(`packetsLive`, {
       type: `response`,
       value: [
-        `live`,
+        `packetsLive`,
         packets,
       ]
     })   
@@ -266,6 +275,10 @@ function broadcastPackets(packets) {
   }
 }
 
+/**
+ * ### Broadcast multiple connections at once
+ * @param {Array<Object>} connections All connections to be broadcasted
+ */
 function broadcastConnections(connections) {
   try {
     connection.broadcast(`connectionsLive`, {
@@ -280,6 +293,10 @@ function broadcastConnections(connections) {
   }
 }
 
+/**
+ * ### Broadcast multiple advertisers at once
+ * @param {Array<Object>} advertisers All advertisers to be broadcasted
+ */
 function broadcastAdvertisers(advertisers) {
   try {
     connection.broadcast(`advertisersLive`, {
@@ -294,7 +311,10 @@ function broadcastAdvertisers(advertisers) {
   }
 }
 
-
+/**
+ * ### Broadcast multiple issues at once
+ * @param {Array<Object>} issues All issues to be broadcasted
+ */
 function broadcastIssues(issues) {
   try {
     connection.broadcast(`issuesLive`, {
@@ -306,54 +326,5 @@ function broadcastIssues(issues) {
     })   
   } catch (err) {
     console.warn(`Failed to broadcast issues:`, err)
-  }
-}
-
-function sendLivePacketSummary(socketId, packet) {
-  return (packet) => {
-    let simplePacket = packet.getInfo()
-    connection.send({
-      type: `response`,
-      value: [
-        `live`,
-        [simplePacket],
-      ]
-    })
-  }
-}
-
-function sendConnections(socketId, connections) {
-  (connections) => {
-    connection.send({
-      type: `response`,
-      value: [
-        `connectionsLive`,
-        connections,
-      ]
-    })
-  }
-}
-
-function sendAdvertisers(socketId, advertisers) {
-  (advertisers) => {
-    connection.send({
-      type: `response`,
-      value: [
-        `advertisersLive`,
-        advertisers,
-      ]
-    })
-  }
-}
-
-function sendIssues(socketId, issues) {
-  (issues) => {
-    connection.send({
-      type: `response`,
-      value: [
-        `issuesLive`,
-        issues,
-      ]
-    })
   }
 }
