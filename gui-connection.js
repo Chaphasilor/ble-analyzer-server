e54eaec3228ede053caf0f93a544f5051082e84a
Chaphasilor <ppp.u@web.de>
2021-03-21 20:51:29 +0100

    GAP (#4)
    * added unsubscribe support
    
    - improved stability
    
    * added more test scenarios
    
    * detect connections even if start is missing
    
    * parse advertising data
    
    - tshark doesn't export all the advertising data to JSON, so manual parsing is required
    - for full and short local name, the value is also  parsed into an ASCII string
    
    * refactoring and documentation
    
    * save short and complete local name to advertisers
    
    * documentation
    
    - added comments and JSDoc
    - did some slight refactoring
    - improved logging
    
    * finished comments for index.js
    
    * finished comments for gui-connection.js
    
    * finished comments for parse.js
    
    * added comments to packet.js
    
    * finished comments for packet.js
    
    * added better support for "Flags" advertising data
    
    * small improvements

--- gui-connection.js
+++ gui-connection.js
@@ -4,38 +4,44 @@ const uuid = require(`uuid`).v4
 
 module.exports = class GuiConnection extends EventEmitter {
 
+  /**
+   * ### Creates a new WebSocket server, allowing clients to connect to the backend
+   * @param {http.Server} server The existing http server to bind the WebSocket server to
+   */
   constructor(server) {
 
-    super()
+    super() // initialize the underlying event emitter
 
+    // create a new WebSocket server from the passed http server
     this.wss = new WebSocket.Server({
       server,
     })
-    this.sockets = new Map()
-    this.subscriptions = {}
+    this.sockets = new Map() // maps socket IDs to the actual sockets
+    this.subscriptions = {} // stores all commands and their respective subscribe clients
 
     this.wss.on(`listening`, () => {
-      console.log(`Websocket server ready and listening`)
+      console.log(`Websocket server ready and listening on port ${process.env.PORT}`)
+      this.emit(`ready`)
     })
     
+    // handle a new client connection
     this.wss.on(`connection`, (socket, request) => {
 
-      let socketId = uuid()
-      socket.send(JSON.stringify(socketId))
+      let socketId = uuid() // generate a unique id for the client/socket
+      socket.send(JSON.stringify(socketId)) // send the id back to the client, could be useful in the future
     
-      this.emit(`ready`)
-      this.sockets.set(socketId, socket)
+      this.sockets.set(socketId, socket) // store the socket for future reference
+      this.emit(`new-client`, socketId) // notify the consumer of the GuiConnection class about the new client connection, along with its ID
       
+      // handle messages received from the client
       socket.on(`message`, (data) => {
     
         let parsed
         try {
           parsed = JSON.parse(data)
         } catch (err) {
-          console.error(`err:`, err);
+          console.error(`Failed to parse message from client:`, err);
         }
-        
-        console.log(`data:`, data)
         
         switch (parsed.type) {
           case `command`:
@@ -47,43 +53,48 @@ module.exports = class GuiConnection extends EventEmitter {
             break;
         }
         
-        // this.emit(`message`, data)
-    
       })
     
       socket.on(`error`, (err) => {
-        console.error(`err:`, err)
+        console.error(`Error from socket '${socketId}':`, err)
       })
     
       socket.on(`close`, (code, reason) => {
 
-        console.log(`this.sockets:`, this.sockets)
-        this.sockets.delete(socketId)
-        console.log(`this.sockets:`, this.sockets)
-        console.log(`Socket closed with code ${code}, reason:`, reason)
-        this.emit(`close`)
+        this.unsubscribe(socketId) // make sure to delete all subscriptions for that client/socket before deleting it
+        this.sockets.delete(socketId) // delete the reference to the socket
+        console.debug(`this.sockets:`, this.sockets)
+        console.warn(`Socket closed with code ${code}, reason:`, reason)
+        this.emit(`close`, socketId) // notify the consumer of the GuiConnection class about the closed socket, including its (former) ID
 
       })
       
     })
     
+    // could be that this old handler isn't cleared before the new handler is added when a connection is established. welp.
     this.wss.on(`error`, (err) => {
-    
-      console.error(`err:`, err)
-      
+      console.error(`Error with the websocket:`, err)
     })
     
   }
 
+  /**
+   * ### Sends a reply to a client
+   * Currently the payload is just forwarded to the client (after being converted to a JSON string)  
+   * This could be improved by building the actual reply inside this method and only accepting certain parameters of the reply, like the packets to be sent or the name of the command (look at the `API.send()` method inside the frontend's API class for an example)
+   * @param {String} socketId The ID of the client connection
+   * @param {Object} payload The reply to be sent to the client
+   */
   send(socketId, payload) {
     
-    if (payload === undefined) {
+    // check if one of the parameters is omitted
+    if (socketId === undefined || payload === undefined) {
       throw new Error(`Missing socket ID or payload!`)
     }
 
-    const stringifiedPayload = JSON.stringify(payload)
+    const stringifiedPayload = JSON.stringify(payload)  // convert the object to a JSON string to send over the socket
 
-    let socket = this.sockets.get(socketId)
+    let socket = this.sockets.get(socketId) // load the socket by using the provided ID
     if (!socket) {
       throw new Error(`Socket with id '${socketId} not found!'`)
     }
@@ -91,24 +102,32 @@ module.exports = class GuiConnection extends EventEmitter {
       throw new Error(`Socket '${socketId}' isn't ready yet!`)
     }
     
-    this.sockets.get(socketId).send(stringifiedPayload)
+    socket.send(stringifiedPayload) // send the payload string
     
   }
 
+  /**
+   * ### Broadcasts a reply to a specific command to all subscribed clients
+   * @param {String} command The command to which this broadcast replies
+   * @param {Object} payload The reply to be broadcasted to all subscribed clients
+   */
   broadcast(command, payload) {
 
     if (command === undefined || payload === undefined) {
       throw new Error(`Missing command name or payload!`)
     }
 
-    // console.info(`Broadcasting`)
+    // console.info(`Broadcasting...`)
 
-    const stringifiedPayload = JSON.stringify(payload)
+    const stringifiedPayload = JSON.stringify(payload) // convert the object to a JSON string to send over the socket
       
+    // check if the command exists inside the subscriptions object. if not, no client has been subscribed to this command yet
     if (this.subscriptions[command]) {
 
+      // get all socket IDs subscribed to that command and send the payload to each of them
       this.subscriptions[command].forEach(socketId => {
 
+        // additional error handling just in case
         let socket = this.sockets.get(socketId)
         if (!socket) {
           throw new Error(`Socket with id '${socketId} not found!'`)
@@ -117,7 +136,7 @@ module.exports = class GuiConnection extends EventEmitter {
           throw new Error(`Socket '${socketId}' isn't ready yet!`)
         }
         
-        this.sockets.get(socketId).send(stringifiedPayload)
+        socket.send(stringifiedPayload)
         
       })
 
@@ -125,6 +144,14 @@ module.exports = class GuiConnection extends EventEmitter {
     
   }
 
+  /**
+   * ### Subscribes a client/socket to all broadcasts for a specific command  
+   *   
+   * Subscriptions are handled by the outside for now (message event -> consumer of the GuiConnection class handles the message (command, etc.) -> calls this `subscribe()`-method)  
+   * This should be changed in the future to make subscription management simpler
+   * @param {String} socketId The ID of the client connection
+   * @param {String} command The command to subscribe the client to
+   */
   subscribe(socketId, command) {
 
     if (this.subscriptions[command]) {
@@ -132,14 +159,40 @@ module.exports = class GuiConnection extends EventEmitter {
     } else {
       this.subscriptions[command] = [socketId]
     }
+
+    console.log(`Subscribed '${socketId}' to '${command}'!`)
     
   }
 
-  unsubscribe(socketId, command) {
+  /**
+   * ### Unsubscribes a client/socket from all broadcasts for a specific command  
+   *   
+   * (Un)subscriptions are handled by the outside for now (message event -> consumer of the GuiConnection class handles the message (command, etc.) -> calls this `unsubscribe()`-method)  
+   * This should be changed in the future to make subscription management simpler
+   * @param {String} socketId The ID of the client connection
+   * @param {String} [command] The command to unsubscribe the client from
+   */
+  unsubscribe(socketId, command = null) {
 
-    if (this.subscriptions[command]) {
-      this.subscriptions[command] = this.subscriptions[command].filter(x => x !== socketId)
+    // if no command is specified, unsubscribe from all commands
+    if (!command) {
+      
+      Object.keys(this.subscriptions).forEach(command => {
+        console.debug(`command:`, command)
+        console.debug(`this.subscriptions:`, this.subscriptions)
+        this.subscriptions[command] = this.subscriptions[command].filter(x => x !== socketId) // filter out this socket from the subscribed sockets
+      })
+      
+    } else {
+
+      // unsubscribe from a single command
+      if (this.subscriptions[command]) {
+        this.subscriptions[command] = this.subscriptions[command].filter(x => x !== socketId) // filter out this socket from the subscribed sockets
+      }
+
     }
+
+    console.log(`Unsubscribed '${socketId}' from '${command}'!`)
     
   }
   
--- index.js
+++ index.js
@@ -1,33 +1,62 @@
 const http = require(`http`)
-require(`dotenv`).config()
 
-const PacketParser = require(`./parse`)
-const GuiConnection = require(`./gui-connection`)
-const Packet = require(`./packet`)
-
-let server = http.createServer()
-server.listen(process.env.PORT)
-let connection = new GuiConnection(server)
-let parser = new PacketParser()
-
-
-connection.on(`ready`, () => {
-
-  console.log(`Connection with GUI established`)
-
-  connection.on(`command`, handleCommand)
-
-  connection.on(`close`, () => {
-    console.log(`unlinking listener`)
-    connection.off(`command`, handleCommand)
-  })
-
+require('dotenv').config(); // loads environment variables from `.env` files
+// improve logging capabilities: timestamps, log type, set log level, etc.
+const betterLogging = require(`better-logging`)
+betterLogging(console, {
+  messageConstructionStrategy: betterLogging.MessageConstructionStrategy.FIRST,
 })
 
+switch (process.env.environment) {
+  case `development`:
+    console.logLevel = 3 // level 3 includes regular logs and everything from level 2
+    break;
+  case `debug`:
+    console.logLevel = 4 // level 4 includes all logs, including debug
+    break;
+  default:
+    console.logLevel = 2 // level 2 and below include info, warn and error
+    break;
+}
+
+// import helper classes
+const PacketParser = require(`./parse`)
+const GuiConnection = require(`./gui-connection`);
+const Packet = require('./packet');
+
+// create a http server to bind the websocket server to
+// the websocket server *could* create its own server, but then it can't shared a port with other http-server stuff
+// using a plain http server makes deployment easier, because we don't need to keep track of the WS port - just use the standard (http) port!
+let server = http.createServer()
+server.listen(process.env.PORT)
+let connection = new GuiConnection(server) // initialize the GuiConnection class, which includes all WebSocket-related things
+let parser = new PacketParser() // initialize the PacketParser class, which handles converting pcap-input into parsed JS objects
+
+// emitted when the WebSocket server is ready to accept connections
+connection.on(`ready`, () => {
+  // register a handler function for messages of type `command`
+  connection.on(`command`, handleCommand)
+})
+
+// fired whenever a client connects to the websocket
+connection.on(`new-client`, (socketId) => {
+  console.info(`Connection with client GUI established!\n  ID:`, socketId)
+})
+
+/**
+ * ### Handles messages of type `command` received from clients
+ * Differentiates between all different types of commands and responds with the requested data
+ * @param {String} socketId The ID of the client/socket connection. Needed to respond to the correct client
+ * @param {Array} command The command sent by the client. Index 0 contains the name of the command, all other indices include various payloads
+ */
 function handleCommand(socketId, command) {
 
   console.log(`Received command from GUI:`, command)
 
+  /**
+   * ### Command end reply
+   * @returns a reply telling the client the command has been handled completely
+   */
   let end = () => {
     return {
       type: `commandEnd`,
@@ -37,6 +66,17 @@ function handleCommand(socketId, command) {
     }
   }
   
+  // a reply containing the data requested by the client
+  // multiple responses can be sent per command
+  // each command **has** to be finalized with an `end()` reply, once there is no more data to send
+  /**
+   * ### Command response reply
+   * 
+   * Multiple responses can be sent per command  
+   * Each command **has** to be finalized with an `end()` reply, once there is no more data to send
+   * @param {Object} payload the data requested by the client
+   * @returns a reply containing the data requested by the client
+   */
   let response = (payload) => {
     return {
       type: `response`,
@@ -47,28 +87,39 @@ function handleCommand(socketId, command) {
     }
   }
 
-  let error = (reason) => {
+  /**
+   * ### Command error reply
+   * 
+   * A reply telling the client that there has been an error while fulfilling the command  
+   * If the error is fatal, the command has to be ended by additionally sending an `end()` reply
+   * @param {String} message a message describing what went wrong
+   * @returns 
+   */
+  let error = (message) => {
     return {
       type: `error`,
       value: [
         command[0],
-        reason,
+        message,
       ]
     }
   }
 
+  // index 0 is the type/name of the command
   switch (command[0]) {
+    // sends all packets in simple format
     case `sendAll`:
 
-      console.log(`parser.packetBuffer.toArray().length:`, parser.packetBuffer.toArray().length);
       connection.send(socketId, response(parser.packetBuffer.toArray().map(packet => packet.getInfo()))) // sends all packets inside the parser.packetBuffer in simple format
       connection.send(socketId, end())
       
       break;
+    // sends a single packet. requires at least one payload (the packet id), accepts an additional payload (the packet format: `simple`, `full` or `raw`). if no format is specified it defaults to `simple`
     case `send`:
 
       if (!(command[1] >= 0)) {
         connection.send(socketId, error(`Missing packet ID while requesting specific packet!`))
+        connection.send(end())
       }
 
       let foundPacket = parser.packetBuffer.toArray().find(packet => {
@@ -77,75 +128,79 @@ function handleCommand(socketId, command) {
 
       if (foundPacket) {
 
-        console.log(`foundPacket:`, foundPacket);
+        console.debug(`foundPacket:`, foundPacket);
+        // if `command[2]` is undefined it returns the default format of `Packet.getInfo()`
         connection.send(socketId, response(foundPacket.getInfo(command[2])))
         connection.send(socketId, end())
         
       } else {
 
-        console.log(`Requested packet not found!`)
+        console.warn(`Couldn't find packet ${command[1]}, requested by client '${socketId}'!`)
         connection.send(error(`Packet with id ${command[1]} not found!`))
+        connection.send(end())
 
       }
     
       break;
 
-    case `live`:
+    // subscribes a client to live packets
+    case `packetsLive`:
 
-      // parser.off(`packet`, sendLivePacketSummary(socketId)) // make sure any previous handler is removed before attaching a new handler
-      // parser.on(`packet`, sendLivePacketSummary(socketId))
-
-      connection.subscribe(socketId, command[0])
+      connection.subscribe(socketId, command[0]) // adds a new subscription inside the GuiConnection, so that broadcasts for `packetsLive` are forwarded to this client
       
       break;
 
+    // subscribes a client to live connections
     case `connectionsLive`:
 
-      // parser.off(`new-connection`, sendConnections(socketId)) // make sure any previous handler is removed before attaching a new handler
-      // parser.on(`new-connection`, sendConnections(socketId))
-
-      connection.subscribe(socketId, command[0])
+      connection.subscribe(socketId, command[0]) // adds a new subscription inside the GuiConnection, so that broadcasts for `connectionsLive` are forwarded to this client
       
       break;
 
+    // sends all known connections that exist or have existed to the client
     case `connections`:
 
-      console.log(`parser.connections:`, parser.connections);
-      connection.send(socketId, response([...parser.connections.values()]))
+      console.debug(`parser.connections:`, parser.connections);
+      connection.send(socketId, response([...parser.connections.values()])) // `parser.connections` is a Map, so the spread operator (`...`) is used to convert it into an array
       connection.send(socketId, end())
 
       break;
 
+    // subscribes a client to live advertisers
     case `advertisersLive`:
 
-      // parser.off(`new-advertiser`, sendAdvertisers(socketId)) // make sure any previous handler is removed before attaching a new handler
-      // parser.on(`new-advertiser`, sendAdvertisers(socketId))
-
-      connection.subscribe(socketId, command[0])
+      connection.subscribe(socketId, command[0]) // adds a new subscription inside the GuiConnection, so that broadcasts for `advertisersLive` are forwarded to this client
       
       break;
 
+    // sends all known advertisers that exist or have existed to the client
     case `advertisers`:
 
-      console.log(`parser.advertisers:`, parser.advertisers);
-      connection.send(socketId, response([...parser.advertisers.values()]))
+      connection.send(socketId, response([...parser.advertisers.values()])) // `parser.advertisers` is a Map, so the spread operator (`...`) is used to convert it into an array
       connection.send(socketId, end())
 
       break;
 
+    // subscribes a client to live issues
     case `issuesLive`:
 
-      // parser.off(`new-issue`, sendIssues(socketId)) // make sure any previous handler is removed before attaching a new handler
-      // parser.on(`new-issue`, sendIssues(socketId))
-
-      connection.subscribe(socketId, command[0])
+      connection.subscribe(socketId, command[0]) // adds a new subscription inside the GuiConnection, so that broadcasts for `issuesLive` are forwarded to this client
       
       break;
 
+    // sends all detected issues to the client
     case `issues`:
 
-      console.log(`parser.issues:`, parser.issues);
       connection.send(socketId, response(parser.issues))
+      connection.send(socketId, end())
+
+      break;
+
+    // used to end a subscription to a live/broadcast command
+    case `end`:
+
+      connection.unsubscribe(socketId, command[1]) // unsubscribe the client inside GuiConnection so that broadcasts aren't forwarded anymore
+      connection.send(socketId, response(`Success`))
       connection.send(socketId, end())
 
       break;
@@ -156,133 +211,131 @@ function handleCommand(socketId, command) {
   
 }
 
-parser.on(`packet`, addPacketToBroadcastQueue)
-// parser.on(`packet`, broadcastPacket)
-parser.on(`new-connection`, broadcastConnections)
-parser.on(`new-advertiser`, broadcastAdvertisers)
-parser.on(`new-issue`, broadcastIssues)
+parser.on(`packet`, addPacketToBroadcastQueue) // packets aren't broadcasted directly, it's more efficient to queue them up and send multiple packets at a regular interval
+parser.on(`new-connection`, broadcastConnections) // attach the broadcast handler for connections
+parser.on(`new-advertiser`, broadcastAdvertisers) // attach the broadcast handler for advertisers
+parser.on(`new-issue`, broadcastIssues) // attach the broadcast handler for issues
 
 let packetQueue = []
-
+// send packets at a regular interval. can be increased if packets are arriving to fast
 setInterval(() => {
 
-  // console.log(`Checking packet queue...`)
-  
   if (packetQueue.length > 0) {
     
-    console.log(`packetQueue:`, packetQueue)
+    console.debug(`packetQueue:`, packetQueue)
     broadcastPackets(packetQueue)
-    packetQueue.length = 0
+    packetQueue.length = 0 // clear the packet queue, very important :)
     
   }
 
-  broadcastConnections([...parser.connections.values()]) // broadcast this regularly to keep # of packets up to date
-  broadcastAdvertisers([...parser.advertisers.values()]) // broadcast this regularly to keep # of packets up to date
-  
 }, 50)
 
+// also regularly update packet counts for connections and advertisers, because else they would only change if a new connection or advertiser is detected, which is rather infrequent 
+setInterval(() => {
+
+  broadcastConnections([...parser.connections.values()]) // broadcast this regularly to keep number of packets inside the connection up to date
+  broadcastAdvertisers([...parser.advertisers.values()]) // broadcast this regularly to keep number of packets from different advertisers up to date
+  
+}, 500)
+
+/**
+ * ### Handler for the `packet` event.  
+ * takes a single packet and adds the packet (simple format) to the broadcast queue
+ * @param {Packet} packet The packet emitted by the event
+ */
 function addPacketToBroadcastQueue(packet) {
   let simplePacket = packet.getInfo()
   packetQueue.push(simplePacket)
 }
 
+/**
+ * Can be used to directly broadcast a single packet  
+ * **not recommended** for medium to high frequency packet streams, but in theory offers the lowest latency
+ * @param {Packet} packet The packet emitted by the event
+ */
 function broadcastPacket(packet) {
   let simplePacket = packet.getSimpleInfo()
-  connection.broadcast(`live`, {
-    type: `response`,
-    value: [
-      `live`,
-      [simplePacket],
-    ]
-  })
-}
-
-function broadcastPackets(packets) {
-  connection.broadcast(`live`, {
-    type: `response`,
-    value: [
-      `live`,
-      packets,
-    ]
-  })  
-}
-
-function broadcastConnections(connections) {
-  connection.broadcast(`connectionsLive`, {
-    type: `response`,
-    value: [
-      `connectionsLive`,
-      connections,
-    ]
-  })
-}
-
-function broadcastAdvertisers(advertisers) {
-  connection.broadcast(`advertisersLive`, {
-    type: `response`,
-    value: [
-      `advertisersLive`,
-      advertisers,
-    ]
-  })
-}
-
-
-function broadcastIssues(issues) {
-  connection.broadcast(`issuesLive`, {
-    type: `response`,
-    value: [
-      `issuesLive`,
-      issues,
-    ]
-  })
-}
-
-function sendLivePacketSummary(socketId, packet) {
-  return (packet) => {
-    let simplePacket = packet.getInfo()
-    connection.send({
+  try {
+    connection.broadcast(`packetsLive`, {
       type: `response`,
       value: [
-        `live`,
+        `packetsLive`,
         [simplePacket],
       ]
-    })
+    })   
+  } catch (err) {
+    console.warn(`Failed to broadcast packet:`, err)    
   }
 }
 
-function sendConnections(socketId, connections) {
-  (connections) => {
-    connection.send({
+/**
+ * ### Broadcast multiple packets at once
+ * @param {Array<Packet>} packets All packets to be broadcasted
+ */
+function broadcastPackets(packets) {
+  try {
+    connection.broadcast(`packetsLive`, {
+      type: `response`,
+      value: [
+        `packetsLive`,
+        packets,
+      ]
+    })   
+  } catch (err) {
+    console.warn(`Failed to broadcast packets:`, err)    
+  }
+}
+
+/**
+ * ### Broadcast multiple connections at once
+ * @param {Array<Object>} connections All connections to be broadcasted
+ */
+function broadcastConnections(connections) {
+  try {
+    connection.broadcast(`connectionsLive`, {
       type: `response`,
       value: [
         `connectionsLive`,
         connections,
       ]
-    })
+    })   
+  } catch (err) {
+    console.warn(`Failed to broadcast connections:`, err)    
   }
 }
 
-function sendAdvertisers(socketId, advertisers) {
-  (advertisers) => {
-    connection.send({
+/**
+ * ### Broadcast multiple advertisers at once
+ * @param {Array<Object>} advertisers All advertisers to be broadcasted
+ */
+function broadcastAdvertisers(advertisers) {
+  try {
+    connection.broadcast(`advertisersLive`, {
       type: `response`,
       value: [
         `advertisersLive`,
         advertisers,
       ]
-    })
+    })   
+  } catch (err) {
+    console.warn(`Failed to broadcast advertisers:`, err)    
   }
 }
 
-function sendIssues(socketId, issues) {
-  (issues) => {
-    connection.send({
+/**
+ * ### Broadcast multiple issues at once
+ * @param {Array<Object>} issues All issues to be broadcasted
+ */
+function broadcastIssues(issues) {
+  try {
+    connection.broadcast(`issuesLive`, {
       type: `response`,
       value: [
         `issuesLive`,
         issues,
       ]
-    })
+    })   
+  } catch (err) {
+    console.warn(`Failed to broadcast issues:`, err)
   }
 }
